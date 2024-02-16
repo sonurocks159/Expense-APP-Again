5a6f35b4b2b0c4164503f58b17afdf7bb55c4b8c
sharazer <sharazerbhatti@yahoo.com>
2024-02-16 17:10:03 -0500

    first commit

--- controllers/transactionsCtrl.js
+++ controllers/transactionsCtrl.js
@@ -30,6 +30,31 @@ const getAllTransaction = async(req,res) => {
     }
 
 }
+const editTransaction = async(req,res)=>{
+    try{
+   
+    
+      await transactionModel.findOneAndUpdate({_id:req.body.transactionId}, req.body.payload );
+      res.status(200).send("Edited successfully")
+       }catch(error){
+        console.log(error)
+        res.status(500).json(error)
+    }
+
+}
+const deleteTransaction = async(req,res)=>{
+    try{
+   
+    
+      await transactionModel.findOneAndDelete({_id:req.body.transactionId})
+      res.status(200).send("Deleted successfully")
+       }catch(error){
+        console.log(error)
+        res.status(500).json(error)
+    }
+
+}
+
 const addTransaction = async (req, res) => {
     try {
         const newTransaction = new transactionModel(req.body);
@@ -44,4 +69,4 @@ const addTransaction = async (req, res) => {
 
 }
 
-module.exports = { getAllTransaction, addTransaction };
+module.exports = { getAllTransaction, addTransaction, editTransaction, deleteTransaction };
--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {addTransaction,getAllTransaction}= require("../controllers/transactionsCtrl")
+const {addTransaction,getAllTransaction, editTransaction, deleteTransaction}= require("../controllers/transactionsCtrl")
 
 
 
@@ -11,6 +11,10 @@ const router  = express.Router()
 //router
 
 router.post('/add-transaction',addTransaction )
+
+router.post('/edit-transaction',editTransaction )
+
+router.post('/delete-transaction',deleteTransaction )
 
 //Get transaction
 router.post('/get-transaction', getAllTransaction)
